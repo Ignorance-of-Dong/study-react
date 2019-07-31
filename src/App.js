fb732eb5776f7de6d092bab9c41c3876c3d37a3d
fanqiang <851524519@qq.com>
2019-07-31 11:58:32 +0800

    'react基础'

--- src/App.js
+++ src/App.js
@@ -1,26 +1,30 @@
-import React from 'react';
+import React, {Component} from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React1
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component  {
+  constructor() {
+    super();
+    this.state = {
+      like : false
+    }
+  }
+  hanleClick() {
+    this.setState(
+      {
+        like : !this.state.like
+      }
+    )
+  }
+  render() {
+    console.log('render 函数触发了')
+    return (
+      <button style="button" style={ this.state.like ? {color : "red"} : {color : 'black'}} 
+      onClick={ ()=>this.hanleClick()}>
+        {this.state.like ? "已赞" : "喜欢"}
+      </button>
+    )
+  }
 }
 
 export default App;
