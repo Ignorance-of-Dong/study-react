cea21f961fa93becb617488011de8a4a37b406b8
fanqiang <851524519@qq.com>
2019-07-31 17:44:14 +0800

    '组件的书写方式'

--- src/App.js
+++ src/App.js
@@ -2,6 +2,39 @@ import React, {Component} from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+import Button, {Nav} from './Button';
+
+// 自定义组件 (类的方式)
+// class Nav extends Component {
+//   constructor() {
+//     super();
+//   }
+//   render() {
+//     return (
+//       <div style={ {color : "white" , background: 'black'} }>fan</div>
+//     )
+//   }
+// }
+
+// 自定义组件 （函数的方式）
+// const Button = function() {
+//   return (
+//     <button>来自button组件</button>
+//   )
+// }
+
+function Fan() {
+  return (
+    <button>来自fan的组件</button>
+  )
+}
+
+const Qiang = ()=> {
+  return (
+    <button>来自qiang的组件</button>
+  )
+}
+
 class App extends Component  {
   constructor() {
     super();
@@ -19,10 +52,16 @@ class App extends Component  {
   render() {
     console.log('render 函数触发了')
     return (
-      <button style="button" style={ this.state.like ? {color : "red"} : {color : 'black'}} 
-      onClick={ ()=>this.hanleClick()}>
-        {this.state.like ? "已赞" : "喜欢"}
-      </button>
+      <div>
+         <Nav />
+         <Button></Button>
+         <Fan></Fan>
+         <Qiang></Qiang>
+        <button style="button" style={ this.state.like ? {color : "red"} : {color : 'black'}} 
+          onClick={ ()=>this.hanleClick()}>
+          {this.state.like ? "已赞" : "喜欢"}
+        </button>
+      </div>
     )
   }
 }
