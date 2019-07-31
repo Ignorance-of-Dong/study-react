434e1c7c47c0579c3c0ed319c3fc87183ed957d5
fanqiang <851524519@qq.com>
2019-07-31 19:48:27 +0800

    '事件监听和箭头函数的this作用域'

--- src/App.js
+++ src/App.js
@@ -4,6 +4,8 @@ import './App.css';
 
 import Button, {Nav} from './Button';
 
+import Input from './Input';
+
 // 自定义组件 (类的方式)
 // class Nav extends Component {
 //   constructor() {
@@ -23,44 +25,48 @@ import Button, {Nav} from './Button';
 //   )
 // }
 
-function Fan() {
-  return (
-    <button>来自fan的组件</button>
-  )
-}
+// function Fan() {
+//   return (
+//     <button>来自fan的组件</button>
+//   )
+// }
 
-const Qiang = ()=> {
-  return (
-    <button>来自qiang的组件</button>
-  )
-}
+// const Qiang = ()=> {
+//   return (
+//     <button>来自qiang的组件</button>
+//   )
+// }
 
 class App extends Component  {
-  constructor() {
-    super();
-    this.state = {
-      like : false
-    }
-  }
-  hanleClick() {
-    this.setState(
-      {
-        like : !this.state.like
-      }
-    )
-  }
+//   constructor() {
+//     super();
+    // this.state = {
+    //   like : false
+    // };
+  // }
+  // hanleClick() {
+    // this.setState(
+    //   {
+    //     like : !this.state.like
+    //   }
+    // )
+    // console.log('你在调用handleClick事件')
+  // }
   render() {
-    console.log('render 函数触发了')
+    // console.log('render 函数触发了')
     return (
       <div>
-         <Nav />
+         {/* <Nav />
          <Button></Button>
          <Fan></Fan>
          <Qiang></Qiang>
         <button style="button" style={ this.state.like ? {color : "red"} : {color : 'black'}} 
           onClick={ ()=>this.hanleClick()}>
           {this.state.like ? "已赞" : "喜欢"}
-        </button>
+        </button> */}
+        {/* <p onClick={()=>this.hanleClick()}>点击看this</p> */}
+
+        <Input></Input>
       </div>
     )
   }
