d77eac804cd650e27b045515367a872fc1446ec7
fanqiang <851524519@qq.com>
2019-08-02 11:44:07 +0800

    '状态提升和方法下方'

--- src/App.js
+++ src/App.js
@@ -2,85 +2,58 @@ import React, {Component} from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-import Button, {Nav} from './Button';
+ class Button extends Component {
+   constructor(props) {
+      super(props);
+   }
 
-import Input from './Input';
 
-import Btn from './components/LikeButton/index'
+   render() {
+     var theme = this.props.theme;
+      return (
+        <div>
+          <button style={ {color : theme} } onClick={()=>this.props.handleClick('red')}>红色</button>
+          <button style={ {color : theme} } onClick={()=>this.props.handleClick('green')}>绿色</button>
+        </div>
+      )
+   }
+ }
 
-// 自定义组件 (类的方式)
-// class Nav extends Component {
-//   constructor() {
-//     super();
-//   }
-//   render() {
-//     return (
-//       <div style={ {color : "white" , background: 'black'} }>fan</div>
-//     )
-//   }
-// }
-
-// 自定义组件 （函数的方式）
-// const Button = function() {
-//   return (
-//     <button>来自button组件</button>
-//   )
-// }
-
-// function Fan() {
-//   return (
-//     <button>来自fan的组件</button>
-//   )
-// }
-
-// const Qiang = ()=> {
-//   return (
-//     <button>来自qiang的组件</button>
-//   )
-// }
+ class Title extends Component {
+   constructor(props) {
+      super(props);
+   }
+   render() {
+     var theme = this.props.theme
+     return (
+       <h2 style={ {color : theme} }>标题</h2>
+     )
+   }
+ }
 
 class App extends Component  {
-//   constructor() {
-//     super();
-    // this.state = {
-    //   like : false
-    // };
-  // }
-  // hanleClick() {
-    // this.setState(
-    //   {
-    //     like : !this.state.like
-    //   }
-    // )
-    // console.log('你在调用handleClick事件')
-  // }
-  render() {
-    console.log('APP组件更新了')
-    return (
-      <div>
-         {/* <Nav />
-         <Button></Button>
-         <Fan></Fan>
-         <Qiang></Qiang>
-        <button style="button" style={ this.state.like ? {color : "red"} : {color : 'black'}} 
-          onClick={ ()=>this.hanleClick()}>
-          {this.state.like ? "已赞" : "喜欢"}
-        </button> */}
-        {/* <p onClick={()=>this.hanleClick()}>点击看this</p> */}
+  constructor(props) {
+    super(props);
+    this.state = {
+      theme : 'red'
+    }
+  }
 
-        {/* <Input></Input> */}
-        {/* <Button></Button>
-        <Btn></Btn>
-        <p onClick={()=>this.setState({})}>点击更新App组件</p> */}
-        <hr />
-        <Nav title="校园">
-            <p>这是一个笑话</p>
-            <h2>这是一个小丑</h2>
-            <Button name="点击一下你就爽了"></Button>
-            <Button name="干就完了"></Button>
-        </Nav>
-      </div>
-    )
+  handleClick(color) {
+    console.log(color)
+    this.setState({
+      theme : color
+    })
+  }
+
+  render() {
+    return (
+        <div>
+          <h5>App</h5>
+          <Title theme={this.state.theme}></Title>
+          <Button theme={this.state.theme} handleClick={(color)=>this.handleClick(color)}></Button>
+        </div>
+    );
   }
 }
 
