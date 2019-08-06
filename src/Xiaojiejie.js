9369009446971f8b59832669d787ab64e82da9d9
fanqiang <851524519@qq.com>
2019-08-06 19:24:45 +0800

    'Fragment的使用'

---
+++ src/Xiaojiejie.js
@@ -0,0 +1,17 @@
+import React, {Component, Fragment} from 'react'
+
+class Xiaojiejie extends Component {
+    render () {
+        return (
+            <Fragment>
+                <div><input /> <button>增加服务</button></div>
+                <ul>
+                    <li>头部按摩</li>
+                    <li>精油推背</li>
+                </ul>
+            </Fragment>
+        )
+    }
+} 
+
+export default Xiaojiejie
