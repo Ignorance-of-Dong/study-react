c670d719a2f99fdf836a94aca4a7bf44f758059e
fanqiang <851524519@qq.com>
2019-08-07 13:55:31 +0800

    '类组件的原理'

---
+++ src-jspang/index.js
@@ -0,0 +1,7 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import App from './App'
+import Xiaojiejie from './Xiaojiejie'
+
+// JSX javascript and xml 在JSX中 遇到 < 就会当成html解析， 遇到 { 就会当做 js 去解析
+ReactDOM.render(<Xiaojiejie />,document.getElementById('root'))
