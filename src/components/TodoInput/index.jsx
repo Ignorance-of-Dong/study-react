36276c1055a08f87bc50ff283440fe60f5a113a5
fanqiang <851524519@qq.com>
2019-08-08 15:14:17 +0800

    '添加回车时间和添加完成之后获取焦点ref'

--- src/components/TodoInput/index.jsx
+++ src/components/TodoInput/index.jsx
@@ -1,4 +1,5 @@
-import React, { Component } from 'react'
+// react里面通过 ref 来获取组件或者dom元素，要使用ref之前必须先调用React.createRef方法来创建一个ref
+import React, { Component, createRef } from 'react'
 
 import PropTypes from 'prop-types'
 
@@ -14,6 +15,13 @@ export default class TodoInput extends Component {
         this.state = {
             inputValue : ''
         }
+        // 在constructor里来创建ref
+        this.inputDom = createRef()
+    }
+    handleKeyUp = (e) => {
+        if (e.keyCode === 13) {
+            this.handleAddClick()
+        }
     }
     handleInputChange = (e) => {
         this.setState ({
@@ -21,12 +29,27 @@ export default class TodoInput extends Component {
         })
     }
     handleAddClick = () => {
+        // 实际的项目中这里还需要对inputValue进行验证,如果验证通过在执行下面的
+        if (this.state.inputValue === '') {
+            return
+        }
         this.props.addTodo(this.state.inputValue)
+        this.setState ({
+            inputValue : ''
+        }, () => {
+            this.inputDom.current.focus()
+        })
     }
     render() {
         return (
             <div>
-                <input type="text" value={this.state.inputValue} onChange={this.handleInputChange}/>  
+                <input
+                 type="text"
+                 value={this.state.inputValue} 
+                 onChange={this.handleInputChange}
+                 onKeyUp={this.handleKeyUp}
+                 ref={this.inputDom}
+                />  
                 <button onClick={this.handleAddClick}>{this.props.btnText}</button>
             </div>
         )
