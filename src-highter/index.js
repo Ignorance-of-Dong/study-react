f90eaba95911b07382d712d93eeb468bdfca9d2e
fanqiang <851524519@qq.com>
2019-08-13 17:46:12 +0800

    'redux的使用'

--- src-highter/index.js
+++ src-highter/index.js
@@ -2,10 +2,8 @@ import React from 'react'
 import { render } from 'react-dom'
 
 import App from './App'
-import store from './store'
-window.store = store
 
 render (
-    <App store={store}/>,
+    <App />,
     document.querySelector('#root')
 )
